638d90f928c58c5e5cf29a7bbfaa542a442120e3
Steven Fulgham <stevenfulgham@Stevens-MacBook-Pro.local>
2017-12-28 09:12:15 -0500

    Configured eslint, configured express server to use EJS, setup babel to transpile ES6 and automated a build script

---
+++ build/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import config from './config';
+import chalk from 'chalk';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {test: 45});
+});
+
+app.set('view engine', 'ejs');
+
+app.listen(config.port, (err) => {
+  if(err){
+    console.info(chalk.red(`An error has occurred listening on port ${config.port}`));
+    return;
+  }
+
+  console.info(chalk.green(`Application running on port ${config.port}`));
+})
