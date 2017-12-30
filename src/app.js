a0586dc97e5fd30527cc23764c5b029799dec06c
Steven Fulgham <stevenfulgham@Stevens-MacBook-Pro.local>
2017-12-29 21:18:25 -0500

    Removed faker from appJs file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components';
-import Faker from './assets/jsLibs/mock.data';
-
-let faker = new Faker();
 
 ReactDom.render(<App />, document.getElementById('root'));
 
